88facde90fc9dcb4ed48cdbdf193c2a68b877f38
Jerry Jia <jerryjia@nyu.edu>
2020-03-07 22:46:17 -0500

    api call to add geofences

--- utilities/data/create_geofences.py
+++ utilities/data/create_geofences.py
@@ -1,14 +1,24 @@
 import requests as re
-url_infzone = "https://api.radar.io/v1/geofences/inf_zones"
+import csv
+
+url = "https://api.radar.io/v1/geofences/"
 API_KEY = "prj_test_sk_4afb042f1bc66611077cc222a421bcd398a8d26f"
+header = {'Authorization': API_KEY}
 
-data = {'api_dev_key': API_KEY,
-        'api_option': 'paste',
-        'api_paste_code': source_code,
-        'api_paste_format': 'python'}
-
-# sending post request and saving response as response object
-r = re.post(url=url_infzone, data=data)
-
-# extracting response text
-pastebin_url = r.text
+with open('time_series_19-covid-Confirmed.csv', mode='r') as file:
+    reader = csv.DictReader(file)
+    line_count = 0
+    for row in reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        data = {
+            'description': str(row["Province/State"]),
+            'tag': 'inf_zone',
+            'type': 'circle',
+            'radius': 10000,
+            'coordinates': [row["Long"], row["Lat"]]
+        }
+        r = re.post(url=url, data=data, headers=header)
+        response = r.text
+        print(response)
