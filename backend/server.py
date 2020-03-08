bc61beb9c61c6fd3cdb630526eb65bd4cf3b2215
Brandon Liang <54skyxenon@gmail.com>
2020-03-08 05:57:21 -0400

    add webhook for Radar

--- backend/server.py
+++ backend/server.py
@@ -3,8 +3,8 @@ import sys
 import json
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from flask import Flask, request
-from classifier import get_risk_score, Response, response_example
+from classifier import get_risk_score, Response
+from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
 
@@ -18,9 +18,34 @@ def main():
         req['options1'] = []
     if 'None of the above' in req['options2']:
         req['options2'] = []
-    
+
     survey_response = Response('contacted', req['options1'] + req['options2'], 10, 0, False)
-    return "Your risk assessment score is: " + "{0:.0%}".format(get_risk_score(survey_response))
+    res = {'probability': "{0:.0%}".format(get_risk_score(survey_response))}
+
+    # if there is a json object in there and it's not empty
+    if os.stat("./location.json").st_size > 0:
+        with open('./location.json', 'r') as myfile:
+            data = json.loads(myfile.read())
+            res = {**res, **data}
+
+    # or else Flask complains if it's served remotely
+    res = jsonify(res)
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
+
+
+# Radar webhook adding location metadata to be retrieved in our app
+@app.route("/radar", methods=['POST'])
+def radar():
+    req = json.loads(request.get_data())
+    with open('./location.json', 'w') as filetowrite:
+        filetowrite.write(json.dumps(req))
+
+    # or else Flask complains if it's served remotely
+    res = make_response('', 200)
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
+
 
 if __name__ == "__main__":
     app.run()
