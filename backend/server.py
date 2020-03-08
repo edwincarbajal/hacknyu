0bd1cf243e117f132e957e56822a48a4b20682f4
Brandon Liang <54skyxenon@gmail.com>
2020-03-08 00:50:07 -0500

    tried to POST to flask, fix some merge conflicts

--- backend/server.py
+++ backend/server.py
@@ -3,13 +3,16 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from classifier import get_risk_score, Response, response_example
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Returns the risk assessment score for COVID19
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['POST'])
 def main():
+    // print(request.json)
     return "Your risk assessment score is: " + "{0:.0%}".format(get_risk_score(response_example))
 
 if __name__ == "__main__":
